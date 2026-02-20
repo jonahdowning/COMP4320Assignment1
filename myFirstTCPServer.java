d0cc44771c20ce4ea9d177bfec7a5d297b0a8e82
Joseph Wilkerson <josephw2030@gmail.com>
2026-02-20 10:49:23 -0600

    Server file w/o object

--- myFirstTCPServer.java
+++ myFirstTCPServer.java
@@ -1,3 +1,24 @@
+import java.io.*;   // for Input/OutputStream
+import java.net.*;  // for Socket and ServerSocket
 public class myFirstTCPServer {
+
+  public static void main(String args[]) throws Exception {
+
+    if (args.length != 1)  // Test for correct # of args
+      throw new IllegalArgumentException("Parameter(s): <Port>");
+
+    int port = Integer.parseInt(args[0]);   // Receiving Port
+	
+    ServerSocket servSock = new ServerSocket(port);
+    Socket clntSock = servSock.accept();
+
+
+    System.out.println("Received Binary-Encoded Friend");
     
+
+    clntSock.close();
+    servSock.close();
+  }
 }
+
+
